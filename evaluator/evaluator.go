b8d91524dac721c9bb96500db7f39326d6f5c9ae
komlow <hokkaidou.milk@gmail.com>
2017-12-02 01:16:03 +0900

    Implement evaluation for boolean type.

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
     "github.com/komlow/writing-an-interpreter-in-go/object"
 )
 
+var (
+    TRUE = &object.Boolean{Value: true}
+    FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
     case *ast.Program:
@@ -13,6 +18,8 @@ func Eval(node ast.Node) object.Object {
         return Eval(node.Expression)
     case *ast.IntegerLiteral:
         return &object.Integer{Value: node.Value}
+    case *ast.Boolean:
+        return nativeBoolToBooleanObject(node.Value)
     }
 
     return nil
@@ -28,3 +35,9 @@ func evalStatements(stmts []ast.Statement) object.Object {
     return result
 }
 
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+    if input {
+        return TRUE
+    }
+    return FALSE
+}
