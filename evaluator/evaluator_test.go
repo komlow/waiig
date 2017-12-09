d583ebc4187a73951a3d8bcaf806daabb4935c85
komlow <hokkaidou.milk@gmail.com>
2017-12-09 17:13:26 +0900

    Implement environment.

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -183,6 +183,10 @@ func TestErrorHandling(t *testing.T) {
             `,
             "unknown operator: BOOLEAN + BOOLEAN",
         },
+        {
+            "foobar",
+            "identifier not found: foobar",
+        },
     }
 
     for _, tt := range tests {
@@ -202,6 +206,23 @@ func TestErrorHandling(t *testing.T) {
     }
 }
 
+func TestLetStatements(t *testing.T) {
+    tests := []struct {
+        input    string
+        expected int64
+    }{
+        {"let a = 5; a;", 5},
+        {"let a = 5 * 5; a;", 25},
+        {"let a = 5; let b = a; b;", 5},
+        {"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+    }
+
+    for _, tt := range tests {
+        testIntegerObject(t, testEval(tt.input), tt.expected)
+    }
+}
+
+
 func testNullObject(t *testing.T, obj object.Object) bool {
     if obj != NULL {
         t.Errorf("object is not NULL. got=%T (%+v)", obj, obj)
@@ -215,8 +236,9 @@ func testEval(input string) object.Object {
     l := lexer.New(input)
     p := parser.New(l)
     program := p.ParseProgram()
+    env := object.NewEnvironment()
 
-    return Eval(program)
+    return Eval(program, env)
 }
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
     "github.com/komlow/writing-an-interpreter-in-go/lexer"
     "github.com/komlow/writing-an-interpreter-in-go/parser"
     "github.com/komlow/writing-an-interpreter-in-go/evaluator"
+    "github.com/komlow/writing-an-interpreter-in-go/object"
 )
 
 const PROMPT = ">> "
@@ -27,6 +28,7 @@ const MONKEY_FACE = `            __,__
 
 func Start(in io.Reader, out io.Writer) {
     scanner := bufio.NewScanner(in)
+    env := object.NewEnvironment()
 
     for {
         fmt.Printf(PROMPT)
@@ -45,7 +47,7 @@ func Start(in io.Reader, out io.Writer) {
             continue
         }
 
-        evaluated := evaluator.Eval(program)
+        evaluated := evaluator.Eval(program, env)
 
         if evaluated != nil {
             io.WriteString(out, evaluated.Inspect())
