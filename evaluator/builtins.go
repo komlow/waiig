b6134a823483d5c07787da246831dec069917fab
komlow <hokkaidou.milk@gmail.com>
2017-12-17 23:04:07 +0900

    Implement builtin printing function.

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+    "fmt"
     "github.com/komlow/writing-an-interpreter-in-go/object"
 )
 
@@ -97,6 +98,15 @@ var builtins = map[string]*object.Builtin{
             newElements[length] = args[1]
 
             return &object.Array{Elements: newElements}
+        },
+    },
+    "puts": &object.Builtin{
+        Fn: func(args ...object.Object) object.Object {
+            for _, arg := range args {
+                fmt.Println(arg.Inspect())
+            }
+
+            return NULL
         },
     },
 }
